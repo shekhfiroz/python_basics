c912e0176059a73b01e4b59e7db9deb1628c64cc
shekh <firozhere@gmail.com>
2017-11-21 08:03:39 +0530

    IMage processing

---
+++ imageProcessing/cnn_tnsorflow_installation .py
@@ -0,0 +1,25 @@
+Open Ana prompt as Admin...
+cd C:\Prog...Files\An...3\
+#pip3 install --upgrade tensorflow
+pip3 install tensorflow # or pip install tensorflow
+python
+import tensorflow as tf
+exit()
+pip3 install keras
+python
+import keras
+
+If u get this err:
+	AttributeError: module 'pandas' has no attribute 'computation'
+exit()
+conda install dask
+python
+import keras
+exit()
+cd C:\Users\admin\Downloads
+
+jupyter notebook
+
+
+
+
